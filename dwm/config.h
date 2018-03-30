ac09faeb6b62939a5716725a8ab4eff92620094f
jishnusen <jishnu1@gmail.com>
2018-03-30 15:43:21 -0700

    st with tmux

--- dwm/config.h
+++ dwm/config.h
@@ -59,7 +59,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", norm_bg, "-nf", norm_fg, "-sb", sel_bg, "-sf", sel_fg, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "st", "-e", "tmux", "attach", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */

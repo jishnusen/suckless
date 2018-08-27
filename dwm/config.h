1cc73d44a85a17de7297b10ee195a4434b99ba3e
jishnusen <jishnu1@gmail.com>
2018-08-26 21:19:40 -0700

    Add movestack patch

--- dwm/config.h
+++ dwm/config.h
@@ -64,6 +64,7 @@ static const char *vupcmd[] = {"pactl", "set-sink-volume", "0", "+5%", NULL};
 static const char *vdowncmd[] = {"pactl", "set-sink-volume", "0", "-5%", NULL};
 static const char *vmutecmd[] = {"pactl", "set-sink-mute", "0", "toggle", NULL};
 
+#include "movestack.c"
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
@@ -75,6 +76,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
